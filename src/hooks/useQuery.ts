e5b019597d289cfbe0b04a25a856599a56e970c4
wangliang <wangliang@houseitech.com>
2024-05-11 18:10:13 +0800

    update

---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,24 @@
+import { useMutation, useQuery as uesReactQuery } from '@tanstack/react-query'
+
+import apiClient from '@/apis/apiClient'
+
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE'
+
+const fnMap = {
+  GET: apiClient.get,
+  POST: apiClient.post,
+  PUT: apiClient.put,
+  DELETE: apiClient.delete,
+}
+type Params = {
+  [key: string]: any
+}
+
+export const useQuery = <T>(type: Method, url: string, params?: Params) => {
+  const query = uesReactQuery<T>({
+    queryKey: [type, url, params],
+    enabled: !!params,
+  })
+
+  return { ...query }
+}
--- vite.config.mts
+++ vite.config.mts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
+        '#': path.resolve(__dirname, 'types'),
       },
     },
     server: {
